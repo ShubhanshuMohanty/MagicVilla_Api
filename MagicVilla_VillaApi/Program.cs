288fff7f74038b370b5fc7c17dd62836b9f7ecb3
shubhanshu <shubhanshumohanty@gmail.com>
2025-08-20 06:39:58 +0530

    create villa repository

--- MagicVilla_VillaApi/Program.cs
+++ MagicVilla_VillaApi/Program.cs
@@ -1,5 +1,7 @@
 using MagicVilla_VillaApi;
 using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Repository.IRepository;
+using MagicVilla_VillaApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddAutoMapper(options =>
 {
     options.AddProfile(new MappingConfig());
---
+++ MagicVilla_VillaApi/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaApi.Models;
+using static MagicVilla_VillaApi.Repository.IRepository.IRepository;
+
+namespace MagicVilla_VillaApi.Repository.IRepository
+{
+    public interface IVillaRepository : IRepository<Villa>
+    {
+
+        Task<Villa> UpdateAsync(Villa entity);
+
+    }
+}
